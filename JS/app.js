102ec43e5c0e18691d13e4d3361881675670d770
JorgeMont <monterrosaspix@gmail.com>
2021-06-25 19:27:19 -0500

    original project done, loads random pokemon on reload

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,7 @@ const getRandomInt = (min, max) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const random = getRandomInt(1, 151);
+  // const random = 155;
   fetchData(random);
 });
 
@@ -19,11 +20,13 @@ const fetchData = async (id) => {
       id: data.id,
       img: data.sprites.other.dream_world.front_default,
       types: data.types,
-      //   hp: data.stats[0].base_stat,
-      //   xp: data.base_experience,
-      //   ataque: data.stats[1].base_stat,
-      //   defensa: data.stats[2].base_stat,
-      //   velocidad: data.stats[5].base_stat,
+      hp: data.stats[0].base_stat,
+      attack: data.stats[1].base_stat,
+      defense: data.stats[2].base_stat,
+      spAtk: data.stats[3].base_stat,
+      spDef: data.stats[4].base_stat,
+      speed: data.stats[5].base_stat,
+      moves: data.moves,
     };
 
     paintCard(pokemon);
@@ -35,14 +38,70 @@ const fetchData = async (id) => {
 const paintCard = (pokemon) => {
   const flex = document.querySelector(".main-flex");
   const template = document.querySelector(".template-card").content;
-  //cloneNode
   const clone = template.cloneNode(true);
   const fragment = document.createDocumentFragment();
 
+  const typeColors = {
+    normal: "var(--normal)",
+    fighting: "var(--fighting)",
+    flying: "var(--flying)",
+    poison: "var(--poison)",
+    ground: "var(--ground)",
+    rock: "var(--rock)",
+    bug: "var(--bug)",
+    ghost: "var(--ghost)",
+    steel: "var(--steel)",
+    fire: "var(--fire)",
+    water: "var(--water)",
+    grass: "var(--grass)",
+    electric: "var(--electric)",
+    psychic: "var(--psychic)",
+    ice: "var(--ice)",
+    dragon: "var(--dragon)",
+    dark: "var(--dark)",
+    fairy: "var(--fairy)",
+  };
+
+  console.log("Pokemon name: ", pokemon.name);
   clone.querySelector(".pokemon__picture img").setAttribute("src", pokemon.img);
-  clone.querySelector(".pokemon__head h1").innerHTML = `${pokemon.name}`;
+  clone.querySelector(".pokemon__head h1").innerHTML =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
   clone.querySelector(".pokemon__head h3").innerHTML = "#" + pokemon.id;
   clone.querySelector(".pk-types__type").innerHTML = pokemon.types[0].type.name;
+  clone.querySelector(".hp").innerHTML = pokemon.hp;
+  clone.querySelector(".atk").innerHTML = pokemon.attack;
+  clone.querySelector(".def").innerHTML = pokemon.defense;
+  clone.querySelector(".spAtk").innerHTML = pokemon.spAtk;
+  clone.querySelector(".spDef").innerHTML = pokemon.spDef;
+  clone.querySelector(".speed").innerHTML = pokemon.speed;
+  clone.querySelector(".movOne").innerHTML = pokemon.moves[0].move.name;
+  clone.querySelector(".movTwo").innerHTML = pokemon.moves[1].move.name;
+  clone.querySelector(".movThree").innerHTML = pokemon.moves[2].move.name;
+  clone.querySelector(".movFour").innerHTML = pokemon.moves[3].move.name;
+
+  // Changes background according to pokemon type
+  clone.querySelector(".pokedex").style.backgroundColor =
+    typeColors[pokemon.types[0].type.name];
+  console.log("Tipos: ", pokemon.types[0].type.name);
+
+  // Tab animations
+  let tabHeader = clone.querySelectorAll(".tab-header")[0];
+  let tabIndicator = clone.querySelectorAll(".tab-indicator")[0];
+  let tabBody = clone.querySelectorAll(".tab-body")[0];
+  let tabsPane = tabHeader.querySelectorAll("div");
+
+  // console.log("tabs", tabsPane);
+  for (let i = 0; i < tabsPane.length; i++) {
+    tabsPane[i].addEventListener("click", function () {
+      tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+      tabsPane[i].classList.add("active");
+      tabBody.getElementsByClassName("active")[0].classList.remove("active");
+      tabBody.getElementsByTagName("div")[i].classList.add("active");
+
+      tabIndicator.style.left = `calc(calc(100% / 2) * ${i})`;
+    });
+  }
+
   fragment.appendChild(clone);
 
   flex.appendChild(fragment);
