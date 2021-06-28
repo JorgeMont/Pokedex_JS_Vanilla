7b7dad65f052095ab7f3ffad3d5537eec8589135
JorgeMont <monterrosaspix@gmail.com>
2021-06-27 23:26:18 -0500

    controll buttons added

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,51 @@
 console.log("Working!");
 
+var pokeId = 1;
+var prev = document.getElementById("prevButton");
+var shuffle = document.getElementById("suffleButton");
+var next = document.getElementById("nextButton");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Poke ID: ", pokeId);
+  fetchData(pokeId);
+});
+
+prev.addEventListener("click", () => {
+  // window.location.reload();
+  if (pokeId === 1) {
+    pokeId = 151;
+    console.log("Poke ID: ", pokeId);
+    fetchData(pokeId);
+  } else {
+    pokeId -= 1;
+    console.log("Poke ID: ", pokeId);
+    fetchData(pokeId);
+  }
+});
+
+shuffle.addEventListener("click", () => {
+  pokeId = getRandomInt(1, 151);
+
+  console.log("Poke ID: ", pokeId);
+  fetchData(pokeId);
+});
+
+next.addEventListener("click", () => {
+  // window.location.reload();
+  if (pokeId === 151) {
+    pokeId = 0;
+    console.log("Poke ID: ", pokeId);
+    fetchData(pokeId);
+  } else {
+    pokeId += 1;
+    console.log("Poke ID: ", pokeId);
+    fetchData(pokeId);
+  }
+});
+
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  const random = getRandomInt(1, 151);
-  // const random = 155;
-  fetchData(random);
-});
 
 const fetchData = async (id) => {
   try {
@@ -35,12 +72,8 @@ const fetchData = async (id) => {
   }
 };
 
+// Paint Pokemon Card With Data
 const paintCard = (pokemon) => {
-  const flex = document.querySelector(".main-flex");
-  const template = document.querySelector(".template-card").content;
-  const clone = template.cloneNode(true);
-  const fragment = document.createDocumentFragment();
-
   const typeColors = {
     normal: "var(--normal)",
     fighting: "var(--fighting)",
@@ -63,31 +96,34 @@ const paintCard = (pokemon) => {
   };
 
   console.log("Pokemon name: ", pokemon.name);
-  clone.querySelector(".pokemon__picture img").setAttribute("src", pokemon.img);
-  clone.querySelector(".pokemon__head h1").innerHTML =
+  document
+    .querySelector(".pokemon__picture img")
+    .setAttribute("src", pokemon.img);
+  document.querySelector(".pokemon__head h1").innerHTML =
     pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-  clone.querySelector(".pokemon__head h3").innerHTML = "#" + pokemon.id;
-  clone.querySelector(".pk-types__type").innerHTML = pokemon.types[0].type.name;
-  clone.querySelector(".hp").innerHTML = pokemon.hp;
-  clone.querySelector(".atk").innerHTML = pokemon.attack;
-  clone.querySelector(".def").innerHTML = pokemon.defense;
-  clone.querySelector(".spAtk").innerHTML = pokemon.spAtk;
-  clone.querySelector(".spDef").innerHTML = pokemon.spDef;
-  clone.querySelector(".speed").innerHTML = pokemon.speed;
-  clone.querySelector(".movOne").innerHTML = pokemon.moves[0].move.name;
-  clone.querySelector(".movTwo").innerHTML = pokemon.moves[1].move.name;
-  clone.querySelector(".movThree").innerHTML = pokemon.moves[2].move.name;
-  clone.querySelector(".movFour").innerHTML = pokemon.moves[3].move.name;
+  document.querySelector(".pokemon__head h3").innerHTML = "#" + pokemon.id;
+  document.querySelector(".pk-types__type").innerHTML =
+    pokemon.types[0].type.name;
+  document.querySelector(".hp").innerHTML = pokemon.hp;
+  document.querySelector(".atk").innerHTML = pokemon.attack;
+  document.querySelector(".def").innerHTML = pokemon.defense;
+  document.querySelector(".spAtk").innerHTML = pokemon.spAtk;
+  document.querySelector(".spDef").innerHTML = pokemon.spDef;
+  document.querySelector(".speed").innerHTML = pokemon.speed;
+  document.querySelector(".movOne").innerHTML = pokemon.moves[0].move.name;
+  document.querySelector(".movTwo").innerHTML = pokemon.moves[1].move.name;
+  document.querySelector(".movThree").innerHTML = pokemon.moves[2].move.name;
+  document.querySelector(".movFour").innerHTML = pokemon.moves[3].move.name;
 
   // Changes background according to pokemon type
-  clone.querySelector(".pokedex").style.backgroundColor =
+  document.querySelector(".pokedex").style.backgroundColor =
     typeColors[pokemon.types[0].type.name];
   console.log("Tipos: ", pokemon.types[0].type.name);
 
   // Tab animations
-  let tabHeader = clone.querySelectorAll(".tab-header")[0];
-  let tabIndicator = clone.querySelectorAll(".tab-indicator")[0];
-  let tabBody = clone.querySelectorAll(".tab-body")[0];
+  let tabHeader = document.querySelectorAll(".tab-header")[0];
+  let tabIndicator = document.querySelectorAll(".tab-indicator")[0];
+  let tabBody = document.querySelectorAll(".tab-body")[0];
   let tabsPane = tabHeader.querySelectorAll("div");
 
   // console.log("tabs", tabsPane);
@@ -101,8 +137,4 @@ const paintCard = (pokemon) => {
       tabIndicator.style.left = `calc(calc(100% / 2) * ${i})`;
     });
   }
-
-  fragment.appendChild(clone);
-
-  flex.appendChild(fragment);
 };
